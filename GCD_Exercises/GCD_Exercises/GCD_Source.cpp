34bca330bf060b6f191d59ed0b21eccf8983cbfa
ndpescobar <ndpescobar@gmail.com>
2019-05-28 11:04:01 -0700

    Update GCD_Source.cpp
    Added several features to allow a user to find many GCD of different sets of numbers.

--- GCD_Exercises/GCD_Exercises/GCD_Source.cpp
+++ GCD_Exercises/GCD_Exercises/GCD_Source.cpp
@@ -1,36 +1,83 @@
 /*	Examples of using GCD algorithms
-	@date 05/23/2019.
+	@version v1.3, 05/28/2019.
 	@author Nicolas Plata-Escobar.
 */
 #include <iostream>
 #include <string>
 using namespace std;
-int a = 618;
-int b = 2530;
-int GCD(int a, int b);
+
+void BruteForceGCD(int a, int b);
+int RecursionGCD(int a, int b);
+
 int main() {
-	cout << "Brute Force finding GCD" << endl;
-	for (int i = a; i > 0; i--) {
-		if ((b/i) == ((float)b/(float)i) && (a/i) == ((float)a/(float)i)) {
-			cout << "good = " << i << endl;
+	int a = 0;
+	int b = 0;
+	string stringA = "";
+	string stringB = "";
+
+	cout << "Welcome!" << endl;
+	while (true) {
+		cout << "Please enter a positive number (Enter -99 to quit): ";
+		getline(cin, stringA);
+		a = stoi(stringA);
+		cout << "a = " << a << endl;
+		cout << "Please enter an even larger positive number (Enter -99 to quit): ";
+		getline(cin, stringB);
+		b = stoi(stringB);
+		cout << "b = " << b << endl;
+		if (a == -99 && b == -99) {
+			break;
+		}
+		else if (a <= 0 || b <= 0) {
+			cout << "Invalid input: numbers must be greater than zero." << endl;
+		}
+		else if (a >= b) {
+			cout << "Invalid Input: first number is larger than second." << endl;
+			cout << "SWITCHING NUMBERS..." << endl;
+			int temp = a;
+			a = b;
+			b = temp;
+			cout << "Now we'll find the Greatest Common Denominator using two different methods." << endl;
+			// Brute Force Method
+			cout << "Brute Force finding GCD" << endl;
+			BruteForceGCD(a, b);
+			// Recursion Method
+			cout << "Recursion finding GCD" << endl;
+			cout << "good = " << RecursionGCD(a, b) << endl;
+		}
+		else if (a < b) {
+		    cout << "Now we'll find the Greatest Common Denominator using two different methods." << endl;
+			// Brute Force Method
+			cout << "Brute Force finding GCD" << endl;
+			BruteForceGCD(a, b);
+			// Recursion Method
+			cout << "Recursion finding GCD" << endl;
+			cout << "good = " << RecursionGCD(a, b) << endl;
 		}
 	}
-
-	cout << "Recursion finding GCD" << endl;
-	cout << "good = " << GCD(a, b) << endl;
-
+    cout << "Double ENTER to end program." << endl;
 	string exit;
 	getline(cin, exit);
 	getline(cin, exit);
 	return 0;
 }
 
-int GCD(int a, int b) {
+void BruteForceGCD(int a, int b) {
+	for (int i = a; i > 0; i--) {
+		if ((b / i) == ((float)b / (float)i) && (a / i) == ((float)a / (float)i)) {
+			cout << "good = " << i << endl;
+		}
+		//cout << "i = " << i << endl;
+	}
+}
+
+int RecursionGCD(int a, int b) {
+	//cout << "a = " << a << " b = " << b << endl;
 	if (a == 0) {
 		return b;
 	}
 	if (b == 0) {
 		return a;
 	}
-	return GCD(b, a % b);
+	return RecursionGCD(b, a % b);
 }
